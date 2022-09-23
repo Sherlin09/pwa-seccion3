7be425205135a64858ed7c8fcbff5fed428d7397
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-09-23 18:43:34 -0500

    correcion de video 21

--- fetch-1.js
+++ fetch-1.js
@@ -1,13 +1,11 @@
 var request = new XMLHttpRequest();
-
-request.open('GET', 'https://regres.in/api/users', true);
+request.open('GET', 'https://reqres.in/api/users', true);
 request.send(null);
 
 request.onreadystatechange = function(state) {
     if (request.readyState === 4) {
         var resp = request.response;
         var respObj = JSON.parse(resp);
-
-        console.log(respObj.page);
+        console.log(respObj);
     }
-};
+}
